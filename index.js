e082e7e89fae3338643f3b24e4575811d958010e
abebe.kayimo@crowdbotics.com <tongisttonji@gmail.com>
2023-03-04 15:16:06 +0300

    Latest update on March4 2023

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 /* FILE STORAGE */
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
-        cb(null, "public/assets");
+        cb(null, path.join(__dirname, 'public/assets'));
     },
     filename: function(req, file, cb){
         cb(null, file.originalname)
